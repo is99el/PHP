62bc6f8bf4806494bb3d0f6e094594f72e90fc8b
ismael <ismaelazaaouaj2004@gmail.com>
2023-01-14 14:40:51 +0100

    H02 AF

--- H02/index.php
+++ H02/index.php
@@ -13,3 +13,17 @@
 //
 //$volledigezin='Mensen '. $a . ' ge'.$b. ' maken,' .'werken niet.';
 //echo $volledigezin;
+
+//$a = 'ry';
+//$b = ',but';
+//$c = 'fail';
+//
+//$volledigezin= 'T'.$a.' and '.$c.$b .' never '.$c.   ' to t'.$a.'.';
+//echo $volledigezin;
+
+//$a = "\"omgaan";
+//$b = "met";
+//$c = "teleu";
+//
+//$volledigezin='De cursus '.$a .' '. $b." " . $c.'rstellingen'.' kan vanavond helaas niet door gaan.';
+//echo $volledigezin;
