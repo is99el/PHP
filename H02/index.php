fe397748ae0c849ff21056f592f1c315edb4ad79
ismael <ismaelazaaouaj2004@gmail.com>
2023-01-14 14:27:35 +0100

    PHP

---
+++ H02/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//$a= 'Ik heb niet gefaald.';
+//$b='die niet werken';
+//$c=10000;
+//
+//$volledigezin= $a . ' Ik heb alleen '. $c .' manieren gevonden '.$b;
+//echo $volledigezin;
+
+//$a='die geen fout';
+//$b="en";
+//$c='niet';
+//
+//$volledigezin='Mensen '. $a . ' ge'.$b. ' maken,' .'werken niet.';
+//echo $volledigezin;
