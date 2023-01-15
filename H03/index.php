039871d4e76f1adc541effb16afc04f156fa98b1
ismael <ismaelazaaouaj2004@gmail.com>
2023-01-15 15:24:06 +0100

    H03 AF

---
+++ H03/index.php
@@ -0,0 +1,64 @@
+<?php
+
+//1
+//for ($i=1; $i<9;$i++){
+//    echo "<img src='img/aap".$i.".jpg'>";}
+
+
+//2
+//$apen= array("aap1.jpg","aap2.jpg","aap3.jpg");
+//    foreach($apen as $aap){
+//        echo "<img src='img/".$aap."'>";
+//    }
+
+
+//3
+
+//for($i=0;$i<10;$i++){
+//    for($j=0;$j<$i;$j++) {
+//        echo "*";
+//    }
+//    echo "*<br>";
+//}
+
+
+//4
+//for($x=35;$x>=25;$x--){
+//    echo "hoppelepee";
+//}
+
+//5
+//for ($i=1; $i<9;$i++){
+//    echo "<img src='img/aap".$i.".jpg'>";}
+
+//6
+
+//$leeftijd=23;
+//$bedrag=10;
+//
+//if ($leeftijd>65){
+//    $bedrag=$bedrag*0.5;
+//}
+//if ($leeftijd<=12){
+//    $bedrag=$bedrag*0.5;
+//}if ($leeftijd<=3){
+//    $bedrag=0;
+//}else{
+//    $bedrag;
+//}
+//echo $bedrag;
+
+//7
+//$a["De spartelkuikens"]=25;
+//$b["De waterbuffels"]=32;
+//$c["Plonsmderin"]=11;
+//$d["Bommetje"]=23;
+//print_r($a);
+
+//8
+//print("De volgende momenten zijn nog beschikbaar:<ul>");
+//foreach($kappersagenda as $afspraak=> $tijd) {
+//    if ($afspraak == " ") {
+//        print("<li>" . $tijd . "</li>");
+//    }
+//}
