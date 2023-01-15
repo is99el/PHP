1762b023318c8d03b13a73770a85655b68fac978
ismael <ismaelazaaouaj2004@gmail.com>
2023-01-15 15:24:20 +0100

    H04 AF

---
+++ H04/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+//function temperature(){
+//    $celsius=20;
+//    $farenheit= $celsius *1.8;
+//    echo 'The temperature in is '.$farenheit.' F.';
+//
+//}
+//
+//temperature();
+
+//function deelBaar()
+//{
+//    $number = 3;
+//    if ($number % 3 == 0) {
+//        return true;
+//
+//    } else {
+//        return false;
+//
+//    }
+//}
+//deelBaar();
+
+//function anderSom(){
+//    $word='patat';
+//    echo strrev($word);
+//
+//}
+//anderSom();
