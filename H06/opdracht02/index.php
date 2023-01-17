7d54626e4bc7e61d1a01a6371a453ef5d715ad11
ismael <ismaelazaaouaj2004@gmail.com>
2023-01-17 14:11:50 +0100

    H06 AF

--- H06/opdracht02/index.php
+++ H06/opdracht02/index.php
@@ -1,14 +1,14 @@
 <?php
-function emailCheck()
-{
-$user="Emailuser";
+
+$user="emailuser";
 $pass="123localhost";
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=login;port=3306', $user, $pass);
-    foreach($conn->query('SELECT * from gegevens') as $row) {
+    $con = new PDO('mysql:host=localhost;dbname=login;port=3306', $user, $pass);
+    foreach($con->query('SELECT * from gegevens') as $row) {
 //        print_r($row);
 
     }
+
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
@@ -16,18 +16,23 @@ try {
 }
 
 
-
-    $sql = "SELECT Email,Wachtwoord FROM gegevens WHERE Email='piet@worldonline.nl'&& Wachtwoord='doetje123'";
-    $result = $conn->query($sql);
-
-    if ($result) {
-       echo 'logged in';
-    } else {
-        echo 'try again';
-    }
+$email=trim($_POST['email']);
+$password=trim($_POST['wachtwoord']);
+//prepare the statement
+$stmt = $con->prepare("SELECT * FROM gegevens WHERE Email=? AND Wachtwoord=?");
+//execute the statement
+$stmt->execute([$email, $password]);
+//fetch result
+$user = $stmt->fetch();
+if ($user) {
+    echo 'Welkom';
+} else {
+    echo 'Sorry, geen toegang!';
 
 }
-emailCheck();
+
+
+
 
 ?>
 
